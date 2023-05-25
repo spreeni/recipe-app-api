162b576290b4949b0f260a4cdb76144cb4ce3a74
Yannic Spreen-Ledebur <yannic.spreen-ledebur@plan-d.com>
2023-05-25 11:34:59 +0200

    Adhere to flake8

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -22,11 +22,11 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
-
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        patched_check.side_effect = [Psycopg2Error] * 2 + \
+                                    [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
